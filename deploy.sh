7389843d4e268eeeaae1a8ef6b0992013abb65b8
guowangyang <guowangyang@tencent.com>
2022-03-01 13:31:27 +0800

    feat: 更新部署脚本、环境变量

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,60 @@
 #!/bin/sh
 set -eux
 
-function getHostInfo() {
-  eval $1=$(awk -F '=' '$1 == "'$1'"  {print $NF}' server.pwd)
+hostName=
+hostPwd=
+project=watch-auto-deploy
+tarFileName=
+
+
+function getTarName() {
+  tarFileName=${project}.tar.gz
 }
 
-fileName=watch-auto-deploy
-tarFileName=${fileName}.tar.gz
 
-getHostInfo hostName
-getHostInfo hostPwd
+function getEnvInfo() {
+  eval $2=$(awk -F '=' '$1 == "'$1'"  {print $NF}' \.env\.local)
+}
 
+function getHostInfo() {
+  getEnvInfo HOST_NAME hostName
+  getEnvInfo HOST_PWD hostPwd
 
-rm -rf ./$tarFileName
-tar --exclude=node_modules/ --exclude=test/ --exclude=output/ --exclude=deploy.sh -zcvf $tarFileName ./*
+  echo $hostName
+  echo $hostPwd
+}
 
-
-if [[ $hostName =~ "155.199" ]];then
+function getTargtDir() {
+  if [[ $hostName =~ "155.199" ]];then
     targetDir=/root/ft_local
-else
+  else
     targetDir=/root/guowangyang/ft_local
-fi
+  fi
+}
 
-expect -c "
-        set timeout 1200;
-        spawn scp -P 36000 -r $tarFileName root@$hostName:$targetDir
-        expect {
-                \"*yes/no*\" {send \"yes\r\"; exp_continue}
-                \"*password*\" {send \"$hostPwd\r\";}
-               }
-expect eof;"
+function zipFile() {
+  rm -rf ./$tarFileName
+  tar --exclude=node_modules/ --exclude=test/ --exclude=output/ --exclude=deploy.sh -zcvf $tarFileName ./*
+}
+
+function uploadFile() {
+  expect -c "
+    set timeout 1200;
+    spawn scp -P 36000 -r $tarFileName root@$hostName:$targetDir
+    expect {
+            \"*yes/no*\" {send \"yes\r\"; exp_continue}
+            \"*password*\" {send \"$hostPwd\r\";}
+            }
+  expect eof;"
+}
+
+function main() {
+  getTarName
+  getHostInfo
+  getTargtDir
+  zipFile
+  uploadFile
+}
+
+main
 
